9988a8d78d112875cbf9be860fa84240ccbaca11
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-25 11:02:42 -0700

    home styling

--- taqueriaeltarasco/src/pages/Home.jsx
+++ taqueriaeltarasco/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import './Home.css';
 import HomeCarousel from '../components/HomeCarousel';
 import aboutSquareImage from '../assets/StoreFront.jpg';
@@ -9,48 +11,66 @@ import tacoPlateImage from '../assets/tacoPlateImage.png';
 
 const Home = () => {
 
-    return (
-        <>
+    const navigate = useNavigate();
+    const handleShowNewPage = (pageString) => {navigate(pageString)};
 
-            <HomeCarousel />
-            <div className='mainHomeOuterContainer'>
-                <div className='mainHomeInnerContainer'>
+return (
+    <>
 
-                    <div className='threeSquares'>
-                        <div className='menuSquare'>
-                            <div className='imageDiv'>
-                                <img className='image' src={cateringImage} />
-                            </div>
-                            <h2 className='overlayText'>Menu</h2>
+        <HomeCarousel />
+        <div className='mainHomeOuterContainer'>
+            <div className='mainHomeInnerContainer'>
+
+                <div className='threeSquares'>
+
+                    <div onClick={()=>handleShowNewPage("Menu")} className='menuSquare'>
+                        <div className='imageDiv'>
+                            <img className='image' src={cateringImage} />
                         </div>
-                        <div className='aboutSquare'>
-                            <div className='imageDiv'>
-                                <img className='image' src={aboutSquareImage} />
-                            </div>
-                            <h2 className='overlayText' >About</h2>
-                        </div>
-                        <div className='cateringSquare'>
-                            <div className='imageDiv'>
-                                <img className='image' src={menuImage} />
-                            </div>
-                            <h2 className='overlayText'> Catering</h2>
-                        </div>
+                        <h2 className='overlayText'>Menu</h2>
                     </div>
 
-                    <div className='welcomeDiv'>
-                        <img className='image' src={tacoPlateImage} />
-                        <div className='welcomeDivText'>
-                            <h1>Welcome!</h1>
+                    <div onClick={()=>handleShowNewPage("About")} className='aboutSquare'>
+                        <div className='imageDiv'>
+                            <img className='image' src={aboutSquareImage} />
                         </div>
+                        <h2 className='overlayText' >About</h2>
                     </div>
 
-
-
+                    <div onClick={()=>handleShowNewPage("Catering")} className='cateringSquare'>
+                        <div className='imageDiv'>
+                            <img className='image' src={menuImage} />
+                        </div>
+                        <h2 className='overlayText'> Catering</h2>
+                    </div>
 
                 </div>
+
+                <div className='welcomeDiv'>
+                    <img className='image' src={tacoPlateImage} />
+                    <div className='welcomeDivText'>
+                        <h1>Welcome!</h1>
+                        <p>Welcome to Taqueria El Tarasco, a culinary oasis nestled in the heart of Wenatchee. Our esteemed establishment is dedicated to curating an exquisite dining experience steeped in the rich tapestry of authentic Mexican cuisine. With a commitment to culinary excellence and a reverence for tradition, we invite you to embark on a delicious journey that celebrates the diverse flavors and heritage of Mexico.</p>
+                        <button>More About Us</button>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className='bottomDiv'>
+                    <p>
+                        At Taqueria El Tarasco, our offerings extend far beyond just tacos, although our tacos are undeniably exceptional! Our menu boasts an array of Mexican culinary essentials and traditional favorites to delight your palate
+                    </p>
+                    <button>View Menu</button>
+                </div>
+
+
+
+
             </div>
-        </>
-    )
+        </div>
+    </>
+)
 };
 
 export default Home
